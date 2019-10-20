0704f498c3548962a1c94b41a155ba27eced6f3d
Carsten König <Carsten.Koenig@hotmail.de>
2019-10-20 10:23:07 +0200

    checking of a rule against a scope

--- src/Rules.purs
+++ src/Rules.purs
@@ -8,11 +8,12 @@ module Rules
   , isCompleted
   , isValidExpr
   , allowNewExprs
+  , isUsableWith
   ) where
 
 import Prelude
 
-import Data.Array (filter)
+import Data.Array (any, filter)
 import Data.Maybe (Maybe(..))
 import Expressions (Expr)
 import Scope (Scope)
@@ -69,3 +70,18 @@ allowNewExprs :: RuleRecipe -> Boolean
 allowNewExprs (Step s) = s.stepAllowNewExprs
 allowNewExprs Failed = false
 allowNewExprs (Succeeded _) = false
+
+-- | this function tries to check if the given rule-recipe
+-- | can possibly be used in the context given by Scope
+-- | please note that it'll not try to find new facts
+-- | in case a step allows creating those it'll return true at
+-- | this point
+isUsableWith :: RuleRecipe -> Scope -> Boolean
+isUsableWith Failed _ = false
+isUsableWith (Succeeded _) _ = true
+isUsableWith (Step s) _ | s.stepAllowNewExprs = true
+isUsableWith (Step s) scope = any tryFindAssignments $ Scope.toArray scope
+  where
+  tryFindAssignments expr | s.stepIsValidExpr expr =
+    isUsableWith (s.stepNext expr) scope
+  tryFindAssignments _ = false
