43ce3388b495f41610bae2175987ca22816011d2
Carsten König <Carsten.Koenig@hotmail.de>
2020-07-22 19:58:39 +0200

    npm audit and another mathjax hack to fix render issues

--- src/Components/MathJaxRenderer.js
+++ src/Components/MathJaxRenderer.js
@@ -1,21 +1,36 @@
 "use strict";
 
 exports.typeSetPage = function () {
-    function cleanUp() {
+    function prepare() {
         var existingNodes = document.querySelectorAll('.math');
         existingNodes.forEach(function (mathNode) {
-            var mathJaxNodes = mathNode.querySelectorAll('.MathJax');
-            if (mathJaxNodes.length > 0 && mathNode.innerText !== "") {
-                // there is a formula text here - so MathJax will replace it
-                // get rid of the already rendered children
+            var mathJaxNodes = Array.from(mathNode.children).filter(e => e.matches('.MathJax'));
+            var allNodeCount = mathNode.children.length;
+            if (mathJaxNodes.length < allNodeCount) {
+                // there is a formula text besides mathJax-content here 
+                // so MathJax will replace the text creating yet another mathjax-text
+                // better get rid of the MathJax-nodes
                 mathJaxNodes.forEach(function (n) { n.remove(); });
             }
         });
     }
 
+    function cleanUp() {
+        var existingNodes = document.querySelectorAll('.math');
+        existingNodes.forEach(function (mathNode) {
+            var mathJaxNodes = Array.from(mathNode.children).filter(e => e.matches('.MathJax'));
+            // remove duplicates - every .math node should only have one .MathJax child-node
+            if (mathJaxNodes.length > 1) {
+                var removed = mathJaxNodes.splice(1, mathJaxNodes.length - 1);
+                removed.forEach(n => n.remove());
+            }
+        });
+    }
+
     try {
-        cleanUp();
+        prepare();
         MathJax.typesetClear();
         MathJax.typeset();
+        cleanUp();
     } catch (e) { }
 }
